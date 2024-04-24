1e9f841cc0b0acba5058f318dce6f164d1e1cb46
pawan <thakunipawan@gmail.com>
2024-04-24 12:45:13 +0530

    chat

---
+++ src/component/ChatBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChatBox = () => {
+  return (
+    <div className=' h-[500px] border border-black bg-slate-200'>ChatBox</div>
+  )
+}
+
+export default ChatBox
--- src/component/Watch.js
+++ src/component/Watch.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
 import CommentContainer from './CommentContainer';
+import ChatBox from './ChatBox';
 
 const Watch = () => {
     const[getVideo]=useSearchParams()
     console.log(getVideo.get('v'));
   return (
-    <div>
+    <div className='flex flex-col'>
+    <div className='flex '>
     <div>
     <iframe width="1200" 
     height="500" 
@@ -15,7 +17,11 @@ const Watch = () => {
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
     referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
-    <div className='font-bold my-7'>
+    <div className='w-full'>
+      <ChatBox/>
+    </div>
+    </div>
+    <div className='font-bold my-7 '>
     <CommentContainer/>
     </div>
     </div>
